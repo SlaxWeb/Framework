a91162b37105a7b6a87f10ef9ea60abee318f8f8
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2015-01-23 12:56:03 +0100

    Move composer autoloading to en

--- public/index.php
+++ public/index.php
@@ -1,10 +1,5 @@
 <?php
 /**
- * Load the system Composer Autoloader.
- */
-require_once "../vendor/autoload.php";
-
-/**
  * Set up some global constants
  */
 // Application directory
@@ -15,11 +10,19 @@ $pubDir = "public";
 // Define FCPATH constant pointing to Fron Controller (this file)
 define("FCPATH", dirname(__FILE__) . "/");
 // Define APPPATH constant pointing to Application directory
-define("APPPATH", FCPATH . "{$appDir}/");
+define("APPPATH", FCPATH . "../{$appDir}/");
 // define PUBPATH constant pointing to Public directory
-define("PUBPATH", FCPATH . "{$pubDir}/");
+define("PUBPATH", FCPATH . "../{$pubDir}/");
 
 /**
  * Load the config
  */
 require_once APPPATH . "config/config.php";
+
+/**
+ * Load the system Composer Autoloader.
+ */
+$loader = require_once FCPATH . "../vendor/autoload.php";
+// Add some stuff to the Autoloader
+$loader->add("Controller\\", APPPATH);
+$loader->add("Model\\", APPPATH);
