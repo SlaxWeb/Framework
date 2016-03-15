f1d9662f2d48a5a9f7468ab7a6ee1543403f6912
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2016-03-15 16:28:25 +0100

    add index.php proposal
    will have to be changed, most of that needs to be moved into Bootstrap
    component, but will require that most of the Bootstrap Application class
    will be rewritten

---
+++ public/index.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * SlaxWeb Framework Front Controller
+ *
+ * This is the Front Controller of the SlaxWeb Framework. It handles all
+ * incoming requests and prints response back to the caller.
+ *
+ * @package   SlaxWeb\Framework
+ * @author    Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @copyright 2016 (c) Tomaz Lovrec
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/slaxweb/
+ * @version   0.3
+ */
+// require the composer autoloader
+require_once __DIR__ . "/../vendor/autoload.php";
+
+// load the application class
+$app = new SlaxWeb\Bootstrap\Application;
+
+// set some application properties
+$app["pubDir"] = __DIR__ . "/";
+$app["appDir"] = "{$app["pubDir"]}../app/";
+
+// register providers
+$app->register(new SlaxWeb\Hooks\Service\Provider);
+$app->register(new SlaxWeb\Config\Service\Provider);
+$app->register(new SlaxWeb\Logger\Service\Provider);
+$app->register(new SlaxWeb\Router\Service\Provider);
+
+// set config component required properties
+$app["configHandler"] = SlaxWeb\Config\Container::PHP_CONFIG_HANDLER;
+$app["configResourceLocation"] = "{$app["appDir"]}Config/";
+
+// initialize the app
+$app->init(
+    $app["config.service"],
+    $app["routeDispatcher.service"],
+    $app["hooks.service"],
+    $app["logger.service"]
+);
+
+// start application
+$app->run($app["request.service"], $app["response.service"]);
+
+// print the output
+$app["response.service"]->send();
